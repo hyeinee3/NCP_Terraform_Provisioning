93b6a79032a045ed723207638a4a5c9c53c846e2
hyeinee3 <86913926+hyeinee3@users.noreply.github.com>
2022-11-29 13:49:15 +0900

    Update vm.tf

--- vm.tf
+++ vm.tf
@@ -1,5 +1,4 @@
 resource "ncloud_server" "public-server" {
-  subnet_no                 = ncloud_subnet.public-subnet.id
   name                      = "qa_ncp_hi"
   server_image_product_code = "SW.VSVR.OS.LNX64.CNTOS.0708.B050"
   server_product_code       = "SVR.VSVR.HICPU.C002.M004.NET.HDD.B050.G002"
